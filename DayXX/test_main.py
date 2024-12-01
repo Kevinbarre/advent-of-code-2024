60a7a15f3a8e9590e8a00e8877283e84050f90ab
Kévin Barré <kevinbarre90@gmail.com>
2024-12-01 16:38:20 +0100

    Add day template

---
+++ DayXX/test_main.py
@@ -0,0 +1,23 @@
+from main import part1, part2
+
+filename = "example.txt"
+
+
+def test_part1():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part1(lines)
+    # Then
+    assert result == 0
+
+
+def test_part2():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part2(lines)
+    # Then
+    assert result == 0
