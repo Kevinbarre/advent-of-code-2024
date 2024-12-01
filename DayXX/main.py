7db570a036028bd012df5301738a6a0b68869e9d
Kévin Barré <kevinbarre90@gmail.com>
2024-12-01 16:38:20 +0100

    Add day template

---
+++ DayXX/main.py
@@ -0,0 +1,14 @@
+def part1(lines):
+    return 0
+
+
+def part2(lines):
+    return 0
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        f_lines = f.read().splitlines()
+
+    print("Part 1 : ", part1(f_lines))
+    print("Part 2 : ", part2(f_lines))
