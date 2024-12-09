edf8b5ac29a959d97e523d115943a708b434aae0
Kévin Barré <kevinbarre90@gmail.com>
2024-12-09 23:12:03 +0100

    Day 09 - Part 2

--- Day09/test_main.py
+++ Day09/test_main.py
@@ -1,4 +1,4 @@
-from main import part1, part2, parse_input, reorganize, calculate_checksum
+from main import part1, part2, parse_input, reorganize, calculate_checksum, reorganize_whole_files, search_next_empty_space
 
 filename = "example.txt"
 
@@ -20,7 +20,7 @@ def test_part2():
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 2858
 
 
 def test_parse_input():
@@ -60,3 +60,66 @@ def test_calculate_checksum():
     result = calculate_checksum(disk_map)
     # Then
     assert result == 1928
+
+
+def test_reorganize_whole_files():
+    # Given
+    disk_map = [
+        "0", "0", ".", ".", ".", "1", "1", "1", ".", ".", ".", "2", ".", ".", ".", "3", "3", "3", ".", "4", "4", ".", "5", "5", "5", "5", ".", "6", "6", "6",
+        "6", ".", "7", "7", "7", ".", "8", "8", "8", "8", "9", "9"
+    ]
+    # When
+    result = reorganize_whole_files(disk_map)
+    # Then
+    assert result == [
+        "0", "0", "9", "9", "2", "1", "1", "1", "7", "7", "7", ".", "4", "4", ".", "3", "3", "3", ".", ".", ".", ".", "5", "5", "5", "5", ".", "6", "6", "6",
+        "6", ".", ".", ".", ".", ".", "8", "8", "8", "8", ".", "."
+    ]
+
+
+def test_search_next_empty_space():
+    # Given
+    disk_map = ["0", "0", ".", "1", ".", ".", ".", "2", "3"]
+    # When
+    result = search_next_empty_space(disk_map, 3, -1)
+    # Then
+    assert result == 4
+
+
+def test_search_next_empty_space_at_end():
+    # Given
+    disk_map = ["0", "0", ".", "1", ".", ".", ".", ".", "."]
+    # When
+    result = search_next_empty_space(disk_map, 5, -1)
+    # Then
+    assert result == 4
+
+
+def test_search_next_empty_space_none_found():
+    # Given
+    disk_map = ["0", "0", ".", "1", ".", ".", ".", "2", "3"]
+    # When
+    result = search_next_empty_space(disk_map, 4, -1)
+    # Then
+    assert result == False
+
+
+def test_search_next_empty_space_after_j():
+    # Given
+    disk_map = ["0", "0", ".", "1", ".", ".", ".", ".", "."]
+    # When
+    result = search_next_empty_space(disk_map, 5, -6)
+    # Then
+    assert result == False
+
+
+def test_calculate_checksum_with_spaces():
+    # Given
+    disk_map = [
+        "0", "0", "9", "9", "2", "1", "1", "1", "7", "7", "7", ".", "4", "4", ".", "3", "3", "3", ".", ".", ".", ".", "5", "5", "5", "5", ".", "6", "6", "6",
+        "6", ".", ".", ".", ".", ".", "8", "8", "8", "8", ".", "."
+    ]
+    # When
+    result = calculate_checksum(disk_map)
+    # Then
+    assert result == 2858
