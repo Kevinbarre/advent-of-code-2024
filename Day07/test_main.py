aaa48a6b6ea20401fb3c4d711cd8e875e2ada6f4
Kévin Barré <kevinbarre90@gmail.com>
2024-12-09 19:51:52 +0100

    Day 07 - Part 2 (Right to Left)

--- Day07/test_main.py
+++ Day07/test_main.py
@@ -1,7 +1,8 @@
 import pytest
 
 from main import part1, part2, parse_input, evaluate, Operators, get_possible_operators, get_total_calibration_result, get_possible_operators2, Operators2, \
-    evaluate2, concat, get_total_calibration_result2
+    evaluate2, concat, get_total_calibration_result2, evaluate_right_to_left, get_possible_operators_right_to_left, unconcat, \
+    get_total_calibration_result_right_to_left
 
 filename = "example.txt"
 
@@ -184,5 +185,74 @@ def test_get_total_calibration_result2():
     ]
     # When
     result = get_total_calibration_result2(equations)
+    # Then
+    assert result == 11387
+
+
+def test_get_possible_operators_right_to_left():
+    # Given
+    length = 2
+    # When
+    result = get_possible_operators_right_to_left(length)
+    # Then
+    assert result == {
+        (Operators2.ADD, Operators2.ADD),
+        (Operators2.ADD, Operators2.MULT),
+        (Operators2.MULT, Operators2.ADD),
+        (Operators2.MULT, Operators2.MULT),
+        (Operators2.ADD, Operators2.CONCAT),
+        (Operators2.CONCAT, Operators2.ADD),
+        (Operators2.CONCAT, Operators2.CONCAT),
+        (Operators2.MULT, Operators2.CONCAT),
+        (Operators2.CONCAT, Operators2.MULT)
+    }
+
+
+def test_unconcat():
+    # Given
+    left = 123456
+    right = 456
+    # When
+    result = unconcat(left, right)
+    # Then
+    assert result == 123
+
+
+@pytest.mark.parametrize("equation, expected",
+                         [((190, [10, 19]), True),
+                          ((3267, [81, 40, 27]), True),
+                          ((83, [17, 5]), False),
+                          ((156, [15, 6]), True),
+                          ((7290, [6, 8, 6, 15]), True),
+                          ((161011, [16, 10, 13]), False),
+                          ((192, [17, 8, 14]), True),
+                          ((21037, [9, 7, 18, 13]), False),
+                          ((292, [11, 6, 16, 20]), True),
+                          # Case when after unconcat we get empty string
+                          ((121, [10, 121]), False)
+                          ])
+def test_evaluate_right_to_left(equation, expected):
+    # Given
+    # When
+    result = evaluate_right_to_left(equation)
+    # Then
+    assert result == expected
+
+
+def test_get_total_calibration_result_right_to_left():
+    # Given
+    equations = [
+        (190, [10, 19]),
+        (3267, [81, 40, 27]),
+        (83, [17, 5]),
+        (156, [15, 6]),
+        (7290, [6, 8, 6, 15]),
+        (161011, [16, 10, 13]),
+        (192, [17, 8, 14]),
+        (21037, [9, 7, 18, 13]),
+        (292, [11, 6, 16, 20]),
+    ]
+    # When
+    result = get_total_calibration_result_right_to_left(equations)
     # Then
     assert result == 11387
