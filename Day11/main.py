65109e0e421d738adabff3189d680ea73c31a564
Kévin Barré <kevinbarre90@gmail.com>
2024-12-11 13:42:06 +0100

    Day 11 - Part 1

---
+++ Day11/main.py
@@ -0,0 +1,54 @@
+from collections import defaultdict, Counter
+
+
+def part1(lines):
+    arrangement = parse_arrangement(lines)
+    arrangement = multiple_steps(arrangement, 25)
+    return count_stones(arrangement)
+
+
+def part2(lines):
+    return 0
+
+
+def parse_arrangement(lines):
+    line = lines[0]
+    return Counter(int(number) for number in line.split())
+
+
+def blink(stone):
+    stone_as_string = str(stone)
+    if stone == 0:
+        return [1]
+    elif len(stone_as_string) % 2 == 0:
+        left, right = int(stone_as_string[:len(stone_as_string) // 2]), int(stone_as_string[len(stone_as_string) // 2:])
+        return [left, right]
+    else:
+        return [stone * 2024]
+
+
+def step(arrangement):
+    next_arrangement = defaultdict(lambda: 0)
+    for stone, count in arrangement.items():
+        new_stones = blink(stone)
+        for new_stone in new_stones:
+            next_arrangement[new_stone] += count
+    return next_arrangement
+
+
+def multiple_steps(arrangement, number_steps):
+    for _ in range(number_steps):
+        arrangement = step(arrangement)
+    return arrangement
+
+
+def count_stones(arrangement):
+    return sum(value for value in arrangement.values())
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        f_lines = f.read().splitlines()
+
+    print("Part 1 : ", part1(f_lines))
+    print("Part 2 : ", part2(f_lines))
---
+++ Day11/test_main.py
@@ -0,0 +1,103 @@
+import pytest
+
+from main import part1, part2, blink, step, multiple_steps, parse_arrangement, count_stones
+
+filename = "example.txt"
+
+
+def test_part1():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part1(lines)
+    # Then
+    assert result == 55312
+
+
+def test_part2():
+    # Given
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    # When
+    result = part2(lines)
+    # Then
+    assert result == 0
+
+
+def test_parse_arrangement():
+    # Given
+    lines = ["125 17"]
+    # When
+    result = parse_arrangement(lines)
+    # Then
+    assert result == {125: 1, 17: 1}
+
+
+def test_blink_zero():
+    # Given
+    stone = 0
+    # When
+    result = blink(stone)
+    # Then
+    assert result == [1]
+
+
+@pytest.mark.parametrize("stone, expected",
+                         [(89, [8, 9]),
+                          (1000, [10, 0])
+                          ])
+def test_blink_even_digits(stone, expected):
+    # Given
+    # When
+    result = blink(stone)
+    # Then
+    assert result == expected
+
+
+@pytest.mark.parametrize("stone, expected",
+                         [(1, [2024]),
+                          (999, [2021976])
+                          ])
+def test_blink_others(stone, expected):
+    # Given
+    # When
+    result = blink(stone)
+    # Then
+    assert result == expected
+
+
+@pytest.mark.parametrize("arrangement, expected",
+                         [({125: 1, 17: 1}, {253000: 1, 1: 1, 7: 1}),
+                          ({253000: 1, 1: 1, 7: 1}, {0: 1, 253: 1, 2024: 1, 14168: 1}),
+                          ({0: 1, 253: 1, 2024: 1, 14168: 1}, {512072: 1, 1: 1, 20: 1, 24: 1, 28676032: 1}),
+                          ({512072: 1, 1: 1, 20: 1, 24: 1, 28676032: 1}, {512: 1, 72: 1, 2024: 1, 2: 2, 0: 1, 4: 1, 2867: 1, 6032: 1}),
+                          ({512: 1, 72: 1, 2024: 1, 2: 2, 0: 1, 4: 1, 2867: 1, 6032: 1},
+                           {1036288: 1, 7: 1, 2: 1, 20: 1, 24: 1, 4048: 2, 1: 1, 8096: 1, 28: 1, 67: 1, 60: 1, 32: 1}),
+                          ({1036288: 1, 7: 1, 2: 1, 20: 1, 24: 1, 4048: 2, 1: 1, 8096: 1, 28: 1, 67: 1, 60: 1, 32: 1},
+                           {2097446912: 1, 14168: 1, 4048: 1, 2: 4, 0: 2, 4: 1, 40: 2, 48: 2, 2024: 1, 80: 1, 96: 1, 8: 1, 6: 2, 7: 1, 3: 1}),
+                          ])
+def test_step(arrangement, expected):
+    # Given
+    # When
+    result = step(arrangement)
+    # Then
+    assert result == expected
+
+
+def test_multiple_steps():
+    # Given
+    arrangement = {125: 1, 17: 1}
+    # When
+    result = multiple_steps(arrangement, 6)
+    # Then
+    assert result == {2097446912: 1, 14168: 1, 4048: 1, 2: 4, 0: 2, 4: 1, 40: 2, 48: 2, 2024: 1, 80: 1, 96: 1, 8: 1, 6: 2, 7: 1, 3: 1}
+
+
+def test_count_stones():
+    # Given
+    arrangement = {2097446912: 1, 14168: 1, 4048: 1, 2: 4, 0: 2, 4: 1, 40: 2, 48: 2, 2024: 1, 80: 1, 96: 1, 8: 1, 6: 2, 7: 1, 3: 1}
+    # When
+    result = count_stones(arrangement)
+    # Then
+    assert result == 22
