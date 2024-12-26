e158f2e270607bf73985015f581ec8d2e53fa7e9
Kévin Barré <kevinbarre90@gmail.com>
2024-12-26 19:30:16 +0100

    Day 22 - Part 2

--- Day22/main.py
+++ Day22/main.py
@@ -1,10 +1,15 @@
+from collections import defaultdict
+
+
 def part1(lines):
     secret_numbers = parse_initial_secret_numbers(lines)
     return sum(generate(secret_number, 2000) for secret_number in secret_numbers)
 
 
 def part2(lines):
-    return 0
+    secret_numbers = parse_initial_secret_numbers(lines)
+    best_prices = build_best_prices(secret_numbers)
+    return max(best_prices.values())
 
 
 def parse_initial_secret_numbers(lines):
@@ -36,6 +41,47 @@ def generate(secret_number, iterations):
     for _ in range(iterations):
         secret_number = next_step(secret_number)
     return secret_number
+
+
+def get_buyer_sequences(secret_number):
+    # Execute the 4 first changes
+    buyer_sequences = {}
+    first_value = secret_number % 10
+    secret_number = next_step(secret_number)
+    second_value = secret_number % 10
+    first = second_value - first_value
+    secret_number = next_step(secret_number)
+    third_value = secret_number % 10
+    second = third_value - second_value
+    secret_number = next_step(secret_number)
+    previous_value = secret_number % 10
+    third = previous_value - third_value
+    secret_number = next_step(secret_number)
+    new_value = secret_number % 10
+    fourth = new_value - previous_value
+    buyer_sequences[(first, second, third, fourth)] = new_value
+    # Iterate for the remaining ones
+    for _ in range(1996):
+        secret_number = next_step(secret_number)
+        previous_value = new_value
+        new_value = secret_number % 10
+        first = second
+        second = third
+        third = fourth
+        fourth = new_value - previous_value
+        key = (first, second, third, fourth)
+        if key not in buyer_sequences:
+            buyer_sequences[key] = new_value
+    return buyer_sequences
+
+
+def build_best_prices(secret_numbers):
+    best_prices = defaultdict(lambda: 0)
+    for secret_number in secret_numbers:
+        buyer_sequences = get_buyer_sequences(secret_number)
+        for sequence, price in buyer_sequences.items():
+            best_prices[sequence] += price
+    return best_prices
 
 
 if __name__ == '__main__':
--- Day22/test_main.py
+++ Day22/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from main import part1, part2, mix, prune, next_step, generate, parse_initial_secret_numbers
+from main import part1, part2, mix, prune, next_step, generate, parse_initial_secret_numbers, get_buyer_sequences, build_best_prices
 
 filename = "example.txt"
 
@@ -17,12 +17,12 @@ def test_part1():
 
 def test_part2():
     # Given
-    with open(filename) as f:
+    with open("example2.txt") as f:
         lines = f.read().splitlines()
     # When
     result = part2(lines)
     # Then
-    assert result == 0
+    assert result == 23
 
 
 def test_parse_initial_secret_numbers():
@@ -90,3 +90,36 @@ def test_generate(secret_number, expected):
     result = generate(secret_number, 2000)
     # Then
     assert result == expected
+
+
+@pytest.mark.parametrize("secret_number, expected_price",
+                         [(1, 7),
+                          (2, 7),
+                          (2024, 9)
+                          ])
+def test_get_buyer_sequences_in(secret_number, expected_price):
+    # Given
+    # When
+    result = get_buyer_sequences(secret_number)
+    # Then
+    assert (-2, 1, -1, 3) in result
+    assert result[(-2, 1, -1, 3)] == expected_price
+
+
+def test_get_buyer_sequences_not_in():
+    # Given
+    secret_number = 3
+    # When
+    result = get_buyer_sequences(secret_number)
+    # Then
+    assert (-2, 1, -1, 3) not in result
+
+
+def test_build_best_prices():
+    # Given
+    secret_numbers = [1, 2, 3, 2024]
+    # When
+    result = build_best_prices(secret_numbers)
+    # Then
+    assert (-2, 1, -1, 3) in result
+    assert result[(-2, 1, -1, 3)] == 23
